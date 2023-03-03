ca23d760deb02de456695fa7e8b030644dfc612d
Kamil Wojcicki <kamilwojcickinjr@gmail.com>
2023-03-03 14:13:32 +0100

    modyfying main project, adding limit to 8 questions

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,17 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScores = false
+    @State private var resetGame = false
     @State private var scoreTitle = ""
     @State private var score = 0
     @State private var wrong = 0
+    private var maxTries = 8
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack{
             RadialGradient(stops: [
-                .init(color: Color(red: 0.1 ,green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                .init(color: Color(red: 0.1 ,green: 0.2, blue: 0.65), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.8)
             ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
             
@@ -35,7 +37,6 @@ struct ContentView: View {
                             .font(.callout.weight(.heavy))
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
-                            
                     }
                     
                     ForEach(0..<3){ number in
@@ -64,28 +65,43 @@ struct ContentView: View {
             }
             .padding()
         }
+        //user have 8 questions and at the end he will get final result.
         .alert(scoreTitle, isPresented: $showingScores){
-            if wrong == 3{
+            if score + wrong == maxTries{
                 Button("Reset", action: resetScore)
             }else{
                 Button("Continue", action: askQuestion)
             }
         }message: {
-            if wrong == 3{
-                Text("Your final score is \(score)")
+            if score + wrong == maxTries{
+                Text("Your final score is \(score)/\(maxTries)")
             }else{
-                if wrong < 3{
-                    Text("Your score is \(score) \n You have \(3 - wrong) try left")
-                }
+                Text("Your score is \(score)")
             }
         }
+        //user have unlimited number of questions but he can tapped wrong answer only 3 times.
+//        .alert(scoreTitle, isPresented: $showingScores){
+//            if wrong == 3{
+//                Button("Reset", action: resetScore)
+//            }else{
+//                Button("Continue", action: askQuestion)
+//            }
+//        }message: {
+//            if wrong == 3{
+//                Text("Your final score is \(score)")
+//            }else{
+//                if wrong < 3{
+//                    Text("Your score is \(score) \n You have \(3 - wrong) try left")
+//                }
+//            }
+//        }
     }
     func flagTapped(_ number: Int){
         if number == correctAnswer{
             scoreTitle = "Correct"
             score += 1
         }else{
-            scoreTitle = "Wrong!"
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
             wrong += 1
         }
         showingScores = true
@@ -98,6 +114,7 @@ struct ContentView: View {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         score = 0
+        wrong = 0
     }
 }
 
